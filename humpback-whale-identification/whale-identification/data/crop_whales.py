e9acd79f7cfccd6fd1e293fb547f18cb350100d8
raghulinfi <raghul@infilect.com>
2019-01-31 12:03:29 +0000

    cropping whales after detection

--- humpback-whale-identification/whale-identification/data/crop_whales.py
+++ humpback-whale-identification/whale-identification/data/crop_whales.py
@@ -2,30 +2,50 @@ import pickle
 import os
 import cv2
 import sys
-sys.path.append('..');
+import shutil
 
+sys.path.append('..');
 from constants import *
 
+def crop_whales(ann, dest_path):
+    """
+    Crops whale locations in an image and saves it to disk
+    """
 
-def crop_whale(img, pt):
+    img_names = [];
+    for i, row in enumerate(ann):
+        img_path, x1, y1, x2, y2, cls = row.split(',');
+        img_name = os.path.split(img_path)[-1];
+        img = cv2.imread(img_path);
+        cropped = img[int(y1):int(y2), int(x1):int(x2)];
+        cv2.imwrite(os.path.join(dest_path, img_name), cropped);	
+        img_names.append(img_name);
 
-	return;
+        if i % 1000 == 0:
+            print('Completed - {}'.format(i));
+
+    print(len(img_names), len(set(img_names)));
+    return;
 
 if __name__ == '__main__':
 
-	modes = ['train', 'test'];
+    """
+    Program to read whale bounding boxes for training whale images, crop
+	and write them to disk
+    """
 
-	with open(os.path.join(DATA_DIR, 'bounding-box', 'bounding-box.pickle'), 'rb') as f:
-		data = pickle.load(f);
+    csv_path = sys.argv[1];
+    dest_path = sys.argv[2];
 
-	print(len(data));
-	print(data.keys());
-	for mode in modes:
-		images = os.listdir(os.path.join(DATA_DIR, mode));
-		print(len(set(images) & set(data.keys())))
-		for file in images:
-			img = cv2.imread(os.path.join(DATA_DIR, mode, file));
-			print(file);
-			print(data[file[1:]]);
-			break;
-		break;
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path);
+    os.makedirs(dest_path);
+
+    with open(csv_path) as f:
+        ann = f.read().split('\n');
+        ann = list(filter(None, ann));
+
+    print('No. of rows - {}'.format(len(ann)));
+    crop_whales(ann, dest_path);
+
+    
